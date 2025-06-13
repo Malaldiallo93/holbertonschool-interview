5e9f1f8aefddbf6e3fc1eb033dc43037bf2422ba
Malaldiallo93 <mohamedmalal93@gmail.com>
2025-06-13 18:14:48 +0200

    update  Simple RegEx code

--- regex/regex.c
+++ regex/regex.c
@@ -1,31 +1,37 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "regex.h"
 
 /**
- * regex_match - Checks if a given pattern matches a given string
- * @str: The string to scan
- * @pattern: The regular expression pattern
+ * regex_match - Regex pattern matchin supporting '*' and '.' wildcards
+ * @str: string 1
+ * @pattern: string 2, can contain '.' and '*' wildcards
  *
- * Return: 1 if the pattern matches the string, 0 otherwise
+ * Description: Regex matching must support the characters . and *
+ *    - . matches any single character
+ *    - * matches zero or more of the preceding character
+ *
+ * Return: 1 if the strings can be considered identical, otherwise return 0
  */
 int regex_match(char const *str, char const *pattern)
 {
-    /* If the pattern is empty, check if the string is also empty */
-    if (*pattern == '\0')
-        return (*str == '\0');
+	if (!*str && !*pattern)
+		return (1);
 
-    /* Check if the first character matches */
-    int first_match = (*str && (*str == *pattern || *pattern == '.'));
+	if (*str && *pattern == '*' && !*(pattern + 1))
+		return (1);
 
-    /* Handle '*' which means zero or more occurrences of the preceding character */
-    if (*(pattern + 1) == '*')
-    {
-        return (regex_match(str, pattern + 2) || /* Ignore '*' pattern */
-                (first_match && regex_match(str + 1, pattern))); /* Match and move */
-    }
-    else
-    {
-        return (first_match && regex_match(str + 1, pattern + 1));
-    }
+	if (*str != *pattern && *pattern && *(pattern + 1) == '*')
+		return (regex_match(str, pattern + 1));
+
+	if (*pattern == '.' || *str == *pattern)
+		return (regex_match(str + 1, pattern + 1));
+
+	if (*pattern == '*')
+	{
+		if (regex_match(str, pattern + 1))
+			return (1);
+		if (*str && regex_match(str + 1, pattern))
+			return (1);
+	}
+
+	return (0);
 }
